e5fba80ddb57be627bbbbef857da41ff54085bf7
Jeremy Flowers <rpgjuwangaming@gmail.com>
2020-08-17 16:27:18 -0500

    Added player stats. Printed player health.

--- Game.cs
+++ Game.cs
@@ -8,7 +8,18 @@ namespace HelloWorld
     {
         public void Run()
         {
+            float health = 100.0f;
+            float healthRegen = 20;
+            string name = Console.ReadLine();
+            bool maxLevelReached = false;
+            int maxLevel = 100;
+            int level = maxLevel;
+            maxLevel = 120;
+            bool ready = true;
+            Console.WriteLine(health);
+            health = health + healthRegen;
 
+            Console.Write("Welcome " + name + " to the world of Ava!");
         }
     }
 }
