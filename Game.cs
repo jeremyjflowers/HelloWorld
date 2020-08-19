00c8a724a19c797e6499d4eb290b05468da5a79c
jeremyjflowers <rpgjuwangaming@gmail.com>
2020-08-19 11:35:59 -0500

    Revamped stats. Printed player role

--- Game.cs
+++ Game.cs
@@ -8,18 +8,23 @@ namespace HelloWorld
     {
         public void Run()
         {
-            float health = 100.0f;
-            float healthRegen = 20;
+            Console.WriteLine("It's a baby boy! What should we name him?");
             string name = Console.ReadLine();
+            Console.WriteLine("Welcome to the world! You do great things in the world, my son! What would you like to be when you come of age?");
+            string role = Console.ReadLine();
+            //This variab;e is used to store health
+            float health = 100.0f;
+            //This variable is used to heal the player
+            float healthRegen = 20;
             bool maxLevelReached = false;
-            int maxLevel = 100;
-            int level = maxLevel;
-            maxLevel = 120;
+            int maxLevel = 120;
+            int level = 10;
             bool ready = true;
-            Console.WriteLine(health);
-            health = health + healthRegen;
+            Console.WriteLine("Player Name: " + name);
+            Console.WriteLine("Player Health: " + health);
+            Console.WriteLine("Player Level: " + level);
+            Console.WriteLine("Player Role: " + role);
 
-            Console.Write("Welcome " + name + " to the world of Ava!");
         }
     }
 }
